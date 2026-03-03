038fece83d55f54525017c5bc4e31b816868f25e
Talha Burak Şimşek <talha.simsek@live.acibadem.edu.tr>
2026-03-03 12:52:38 +0300

    first commit

---
+++ Parcali_bulut/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Parcali_bulut project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Parcali_bulut.settings")
+
+application = get_wsgi_application()
